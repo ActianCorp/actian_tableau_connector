4b3cc539f328566e4cc0684f6a6264d9d1c6f48e
Chris Clark <Chris.Clark@actian.com>
2020-05-15 16:27:44 -0700

    Allow additional ODBC driver options to be specified
    Enter in using form:
    
        OPTION=VALUE;OPTION2=VALUE2
    
    Spaces are not recommended.
    Trailing blank/spaces are NOT supported.

--- actian_odbc/connectionBuilder.js
+++ actian_odbc/connectionBuilder.js
@@ -10,6 +10,8 @@
     }
     else
     {
+        // TODO try using HostName and ListenAddress named params instead of single SERVER
+        // this would allow overrides - Tableau may not support this though :-(
         params["SERVER"] = '@' + attr["server"] + ',' + attr["port"]; // NOTE implicit tcp_ip protocol
         params["UID"] = attr["username"];
         params["PWD"] = attr["password"];
@@ -20,6 +22,25 @@
     {
         params["ServerType"] = params["DATABASE"].slice(slash_index + 1);  // determine server class (skip slash)
         params["DATABASE"] = params["DATABASE"].slice(0, slash_index);  // strip class, leaving only database name
+    }
+
+
+    // support any ODBC connection attribute
+    // specify as OPTION=VALUE;OPTION2=VALUE2
+    var tmp_input = attr[connectionHelper.attributeVendor1].trim()
+    if (tmp_input)
+    {
+        // trim() maybe overkill, better safe than sorry
+        var tmp_list = tmp_input.split(';');
+        for (var key in tmp_list)
+        {
+            var tmp_str_pair = tmp_list[key].trim();
+            if (tmp_str_pair)
+            {
+                var key_value_list = tmp_str_pair.trim().split('=');
+                params[key_value_list[0].trim()] = key_value_list[1].trim();
+            }
+        }
     }
 
     var formattedParams = [];
