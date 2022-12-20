289df402b85b716d302634cf5c0545bb7928523f
hab6 <115743596+hab6@users.noreply.github.com>
2022-12-20 11:25:01 -0600

    New files

---
+++ actian_jdbc/connectionProperties.js
@@ -0,0 +1,14 @@
+(function propertiesbuilder(attr) {
+
+    var props = {};
+
+    props["user"] = attr[connectionHelper.attributeUsername];
+    props["password"] = attr[connectionHelper.attributePassword];
+
+    if (attr[connectionHelper.attributeSSLMode] == "require") {
+        props["ssl"] = "true";
+        props["sslmode"] = "require";
+    }
+
+    return props;
+})
