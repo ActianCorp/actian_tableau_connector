aaa2a720cc71612f1b0bb7893a44ed2ced2b8c7e
Michael Habiger <michael.habiger@actian.com>
2023-08-16 10:17:21 -0500

    Rework connection dialog, other minor changes

--- actian_jdbc/connectionBuilder.js
+++ actian_jdbc/connectionBuilder.js
@@ -1,29 +1,15 @@
 (function dsbuilder(attr)
 {
-    var params = [];
+    var urlBuilder = null;
 
     if (attr["server"].toLowerCase() == "(local)")  // match ODBC implementation, even though JDBC driver does not understand (local)
     {
-        attr["server"] = "localhost";
-        // local connection, no auth specified
-        // port is required
+        urlBuilder = "jdbc:ingres://" + "localhost:" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + ";";
     }
     else
     {
-        params["UID"] = attr["username"];
-        params["PWD"] = attr["password"];
+        urlBuilder = "jdbc:ingres://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + ";";
     }
-
-    var urlBuilder = "jdbc:ingres://" + attr["server"] + ":" + attr["port"] + "/" + attr["dbname"] + ";";
-
-    var formattedParams = [];
-
-    for (var key in params) {
-        formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
-    }
-
-    urlBuilder += formattedParams.join(";")
 
     return [urlBuilder];
-}) 
- 
+})
