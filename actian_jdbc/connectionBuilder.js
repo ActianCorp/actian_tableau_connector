26a7cb823aa6ba033385aadc7a2572937040a9ff
Michael Habiger <michael.habiger@actian.com>
2024-10-10 13:29:09 -0500

    Updates allowing XSD validation to pass

--- actian_jdbc/connectionBuilder.js
+++ actian_jdbc/connectionBuilder.js
@@ -11,7 +11,7 @@
         urlBuilder = "jdbc:ingres://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + ";";
     }
 
-    if (attr["use_select_loops"] == "require")
+    if (attr["v-use-select-loops"] == "require")
     {
         urlBuilder += "SELECT_LOOP=ON;";
     }
